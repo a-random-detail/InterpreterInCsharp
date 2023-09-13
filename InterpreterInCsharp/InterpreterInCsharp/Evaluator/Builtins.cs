72c433049f0ac54fe8436f422978cfd0dfeaa810
Dalton DeMaria <dalton.demaria@gmail.com>
2023-09-12 21:47:31 -0500

    add builtins for array

--- InterpreterInCsharp/InterpreterInCsharp/Evaluator/Builtins.cs
+++ InterpreterInCsharp/InterpreterInCsharp/Evaluator/Builtins.cs
@@ -4,8 +4,70 @@ namespace InterpreterInCsharp.Evaluator;
 
 public class Builtins {
     public static readonly Dictionary<string, MonkeyBuiltin> BuiltinsMap = new() {
-        {"len", new MonkeyBuiltin(Len)}
+        {"len", new MonkeyBuiltin(Len)},
+        {"first", new MonkeyBuiltin(First)},
+        {"last", new MonkeyBuiltin(Last)},
+        {"rest", new MonkeyBuiltin(Rest)},
     };
+
+    private static MonkeyObject Rest(MonkeyObject[] arg)
+    {
+        if (arg.Length != 1)
+        {
+            return new MonkeyError($"wrong number of arguments. got={arg.Length}, want=1");
+        }
+
+        if(arg[0].Type != ObjectType.Array)
+        {
+            return new MonkeyError($"argument to `rest` must be ARRAY, got {arg[0].Type}");
+        }
+        var arr = arg[0] as MonkeyArray;
+        var length = Len(arg) as MonkeyInteger;
+        if (length?.Value > 0)
+        {
+            var newElements = arr?.Elements.Skip(1).ToArray();
+            return new MonkeyArray(newElements);
+        }
+        return new MonkeyNull();
+    }
+
+    private static MonkeyObject Last(MonkeyObject[] arg)
+    {
+        if (arg.Length != 1)
+        {
+            return new MonkeyError($"wrong number of arguments. got={arg.Length}, want=1");
+        }
+        if (arg[0].Type != ObjectType.Array)
+        {
+            return new MonkeyError($"argument to `last` must be ARRAY, got {arg[0].Type}");
+        }
+        var arr = arg[0] as MonkeyArray;
+        var length = Len(arg) as MonkeyInteger;
+        if (length?.Value > 0)
+        {
+            return arr?.Elements[^1] ?? new MonkeyNull();
+        }
+        return new MonkeyNull();
+    }
+
+    private static MonkeyObject First(MonkeyObject[] arg)
+    {
+        if (arg.Length != 1)
+        {
+            return new MonkeyError($"wrong number of arguments. got={arg.Length}, want=1");
+        }
+        if (arg[0].Type != ObjectType.Array)
+        {
+            return new MonkeyError($"argument to `first` must be ARRAY, got {arg[0].Type}");
+        }
+        var arr = arg[0] as MonkeyArray;
+        var length = Len(arg) as MonkeyInteger;
+        if (length?.Value > 0)
+        {
+            return arr?.Elements[0] ?? new MonkeyNull();
+        }
+        return new MonkeyNull();
+    }
 
     private static MonkeyObject Len(MonkeyObject[] arg)
     {
@@ -14,11 +76,12 @@ public class Builtins {
             return new MonkeyError($"wrong number of arguments. got={arg.Length}, want=1");
         }
 
-        if (arg[0].Type == ObjectType.String)
+        var arr = arg[0] as MonkeyArray;
+        return arg[0].Type switch
         {
-            return new MonkeyInteger(arg[0].Inspect().Length);
-        }
-
-        return new MonkeyError($"argument to `len` not supported, got {arg[0].Type}");
+            ObjectType.Array => new MonkeyInteger((arg[0] as MonkeyArray)?.Elements.Count() ?? 0),
+            ObjectType.String => new MonkeyInteger(arg[0].Inspect().Length),
+            _ => new MonkeyError($"argument to `len` not supported, got {arg[0].Type}")
+        };
     }
 }
